0e33059c7f976843b07edc9587f0fa2780a1ff0e
marcelosousa <dipython@gmail.com>
2015-03-31 14:30:13 +0100

    added extract function to API

--- src/Language/SimpleC.hs
+++ src/Language/SimpleC.hs
@@ -12,3 +12,21 @@ import Language.SimpleC.Converter
 import Language.SimpleC.Printer
 --import Language.SimpleC.Flow
 
+parseFile :: FilePath -> IO CTranslUnit
+parseFile f  =
+  do parse_result <- parseCFile (newGCC "gcc") Nothing [] f
+     case parse_result of
+       Left parse_err -> do 
+           parse_result <- parseCFilePre f
+           case parse_result of
+               Left _ -> error (show parse_err)
+               Right ast -> return ast
+       Right ast      -> return ast
+
+extract :: FilePath -> IO SC.Program
+extract f = do ctu <- parseFile f
+               return $ translate ctu
+
+success :: SC.Program -> Bool
+success (SC.Program _) = True
+
--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,8 @@ import Language.C
 import Language.C.System.GCC  -- preprocessor used
 import Language.C.Data.Ident
 
-import qualified Language.SimpleC.AST as SC
+import Language.SimpleC
 import Language.SimpleC.Converter
-import Language.SimpleC.Printer
---import Language.SimpleC.Flow
 
 import System.FilePath
 import System.Directory
@@ -16,28 +14,10 @@ import Test.HUnit
 main :: IO ()
 main = putStrLn "simple-c: a library for simple c"
 
-parseFile :: FilePath -> IO CTranslUnit
-parseFile f  =
-  do parse_result <- parseCFile (newGCC "gcc") Nothing [] f
-     case parse_result of
-       Left parse_err -> do 
-           parse_result <- parseCFilePre f
-           case parse_result of
-               Left _ -> error (show parse_err)
-               Right ast -> return ast
-       Right ast      -> return ast
-
-pp :: FilePath -> IO ()
-pp f = do ctu <- parseFile f
-          print $ translate ctu
-
 printMyAST :: CTranslUnit -> IO ()
 printMyAST ctu = print ctu -- (print . pretty) ctu
 
 myMain fn f = parseFile fn >>= f
-
-success :: SC.Program -> Bool
-success (SC.Program _) = True
 
 -- svcomptest :: FilePath -> IO ()
 svcomptest = do

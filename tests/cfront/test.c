244b66de285ea57d0ba676207f54104e48b12437
marcelosousa <dipython@gmail.com>
2016-06-07 10:04:46 -0400

    working on cfg

---
+++ tests/cfront/test.c
@@ -0,0 +1,18 @@
+// #include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  int i=0;
+
+  if (i > argc) {
+    i++;
+  } else {
+    return i--;
+  }
+
+  while (i < 10) {
+    i++;
+  }
+
+  return i;
+}

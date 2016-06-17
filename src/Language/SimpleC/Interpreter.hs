dba7ab584a8cac90e8355f31ad3173a800dbcc46
marcelosousa <dipython@gmail.com>
2016-06-16 17:40:52 -0700

    working on it

--- src/Language/SimpleC/Interpreter.hs
+++ src/Language/SimpleC/Interpreter.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Language.SimpleC.Interpreter where
+
+import Control.Monad.State.Lazy
 
 import Data.Map (Map)
 import Data.IntMap
@@ -8,7 +10,9 @@ import Data.IntMap
 import Language.SimpleC.AST 
 import Language.SimpleC.Converter
 import Language.SimpleC.Flow
+import Language.SimpleC.Util
 
+{-
 data FrontEnd node 
  = FrontEnd 
  { 
@@ -16,12 +20,11 @@ data FrontEnd node
  , cfgs :: Graphs SymId  node
  , symt :: Map SymId Symbol
  } deriving Show
-
+-}
 
 data MemCell = MemCell
 type Heap = IntMap MemCell
 
-{-
 data Env = 
   Env {
     heap :: Heap
@@ -31,6 +34,8 @@ data Env =
 -- Map Pid Functions
 type Functions = IntMap Function 
 
+data Function = F
+{-
 -- Not sure what a process needs yet.
 -- | Calling context [(Stack, Function)]
 data Function = 
@@ -39,13 +44,11 @@ data Function =
   , code :: Graph 
   , pos  :: Int
   }  
+-}
 
 type Stack = IntMap MemCell 
 
 type EnvState = State Env
-type EnvOp val = State (FlowState ident node) val
-
--}
 
 -- The purpose of the interpreter 
 -- is to load the program and put
@@ -58,5 +61,4 @@ interpreter = undefined
 -- computes an environment/configuration
 -- with the initial declarations
 i_env :: FrontEnd () -> Env
-i_env f@FrontEnd{..} =
-  let 
+i_env f@FrontEnd{..} = undefined
